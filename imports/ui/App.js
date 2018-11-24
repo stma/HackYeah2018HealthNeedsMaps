6616de62598f7413b4b680a5e4ff5a9ac945839b
matyi <matyi@miutcank.hu>
2018-11-24 18:35:08 +0100

    react init

---
+++ imports/ui/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Nyammm</h1>
+                </header>
+
+                <ul>
+                    <li>frontend</li>
+                    <li>backend</li>
+                    <li>db</li>
+                </ul>
+            </div>
+        );
+    }
+}
